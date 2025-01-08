19d3c99c82da3caa97dc374a882a02806842c80e
N-feng <308561157@qq.com>
2025-01-08 14:08:49 +0800

    fix🐛: fix some bug from index page

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body suppressHydrationWarning className={cn("bg-secondary", inter.className)}>
+        <body className={cn("bg-secondary", inter.className)}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <ProModal />
             {children}
