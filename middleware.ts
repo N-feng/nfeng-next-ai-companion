8a70f8f702174739f91984ad9fbc990f3a15d58b
N-feng <308561157@qq.com>
2025-01-12 21:37:04 +0800

    fix🐛: remove publicRoutes

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,6 @@
-import { authMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-// See https://clerk.com/docs/references/nextjs/auth-middleware
-// for more information about configuring your Middleware
-export default authMiddleware({
-  // Allow signed out users to access the specified routes:
-  publicRoutes: ['/api/webhook', '/api/webhook/clerk'],
-});
+export default clerkMiddleware()
 
 export const config = {
   matcher: [
